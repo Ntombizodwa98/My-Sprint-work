73fae712805417a9be357c2f4cddb745f9eb0043
Ntombizodwa Thwala <65090789+Ntombizodwa98@users.noreply.github.com>
2020-06-19 09:23:49 +0200

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,13 @@
+#the string module contains separate constants for lowercase, uppercase letters, digits, and special characters.
+import random, string 
+#defines a function that takes in the password length
+def password(length = 12):
+#concatenates different characters from the string constant
+    chars = string.ascii_letters + string.digits + string.punctuation
+#returns a single combination  of 12 characters from the characters combined above
+    return (''.join(random.choice(chars) for i in range(length)))
+
+#generates a 12-character password
+print(f"Your password is: {password()}") 
+
+
