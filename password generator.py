80c3d938e2a4d48ef04362d9a87fe3e246bd5fc8
Ntombizodwa Thwala <65090789+Ntombizodwa98@users.noreply.github.com>
2020-06-18 23:26:11 -0800

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -4,7 +4,7 @@ import random, string
 def password(length = 12):
 #concatenates different characters from the string constant
     chars = string.ascii_letters + string.digits + string.punctuation
-#returns a single combination  of 12 characters from the characters combined above
+#returns a single combination  of 12 characters from the characters combined above which forms a password
     return (''.join(random.choice(chars) for i in range(length)))
 
 #generates a 12-character password
